0e5b3259e586a00a5c0bfee1f4c3184c247c90ba
rangel <rangel@rangel.com.cn>
2018-06-19 00:11:15 +0800

    Initial commit

---
+++ bos_management/src/main/java/com/itheima/bos_management/BosManagementApplication.java
@@ -0,0 +1,12 @@
+package com.itheima.bos_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BosManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BosManagementApplication.class, args);
+    }
+}
